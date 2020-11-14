049cc7d68d7cd1a9f1920da95bee6e4b72854ea6
pranay_teja <s.pranayteja@gmail.com>
2020-11-14 12:49:31 +0530

    add redirect 401 response

--- src/app/services/auth/token-interceptor.service.ts
+++ src/app/services/auth/token-interceptor.service.ts
@@ -1,18 +1,21 @@
+import { Router } from '@angular/router';
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -20,10 +23,24 @@ export class TokenInterceptorService implements HttpInterceptor {
     // req.headers.append('Authorization', `Bearer ${this.authService.getToken()}`);
     req = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.authService.getToken()}`
-      }
-    })
+        Authorization: `Bearer ${this.authService.getToken()}`,
+      },
+    });
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+          return this.handle410Error(req, next, error);
+        } else {
+          return throwError(error);
+        }
+      })
+    );
+  }
+
+  private handle410Error(req: HttpRequest<any>, next: HttpHandler, error) {
+    this.router.navigate(['/login']);
+    // return next.handle(req);
+    return throwError(error);
   }
 }
